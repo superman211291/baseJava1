eb3fd9a94c7ca0b8739a7c923ac51df254b6396b
Ilya Stroev <ilya_stroev@mail.ru>
2020-10-15 22:52:21 +0300

    add metods realize save,delete,getAll,size,get,clear

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,65 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int count_resume = 0;
 
     void clear() {
+        count_resume = 0;
     }
 
     void save(Resume r) {
+        boolean flag = false;
+        for (int i = 0; i < count_resume; i++) {
+            if (storage[i].uuid.equals(r.uuid)) {
+                flag = true;
+            }
+        }
+        if (flag) {
+            System.out.println("Такое резюме уже есть!");
+        } else {
+            storage[count_resume] = r;
+            count_resume++;
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < count_resume; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < count_resume; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < count_resume; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                break;
+            }
+        }
+        count_resume--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+ /*       Resume[] resume = new Resume[count_resume];
+        for (int i = 0; i < resume.length ; i++) {
+            resume[i]=storage[i];
+        }
+        return resume;*/
+        return Arrays.copyOfRange(storage, 0, count_resume);
     }
 
     int size() {
-        return 0;
+        return count_resume;
     }
 }
--- src/MainArray.java
+++ src/MainArray.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Interactive test for ArrayStorage implementation
