50e06b865ccc8c10410eaf8f3fc6a379e9e295d2
Ilya Stroev <ilya_stroev@mail.ru>
2020-10-27 00:54:10 +0300

    add method save and delete to SortedArrayStorage

--- src/com/urise/webapp/MainTestArrayStorage.java
+++ src/com/urise/webapp/MainTestArrayStorage.java
@@ -1,13 +1,14 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-import com.urise.webapp.storage.ArrayStorage;
+import com.urise.webapp.storage.SortedArrayStorage;
+import com.urise.webapp.storage.Storage;
 
 /**
  * Test for your com.urise.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
-    static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
+    static final Storage ARRAY_STORAGE = new SortedArrayStorage();
 
     public static void main(String[] args) {
         Resume r1 = new Resume();
@@ -16,33 +17,41 @@ public class MainTestArrayStorage {
         r2.setUuid("uuid2");
         Resume r3 = new Resume();
         r3.setUuid("uuid3");
+        Resume r0 = new Resume();
+        r0.setUuid("uuid0");
+        Resume r5 = new Resume();
+        r5.setUuid("uuid5");
 
         ARRAY_STORAGE.save(r1);
-        ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
+        ARRAY_STORAGE.save(r0);
+        ARRAY_STORAGE.save(r0);
+        ARRAY_STORAGE.save(r2);
+//        ARRAY_STORAGE.save(r5);
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
-        System.out.println("Size: " + ARRAY_STORAGE.size());
-
-        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
-
-        printAll();
-        ARRAY_STORAGE.delete(r1.getUuid());
-        printAll();
-        ARRAY_STORAGE.clear();
-        printAll();
-
-        System.out.println("Size: " + ARRAY_STORAGE.size());
-
-        for (int i = 0; i < 10001; i++) {
-            ARRAY_STORAGE.save(new Resume().setUuid("name" + i));
-        }
-    }
-
-    static void printAll() {
-        System.out.println("\nGet All");
-        for (Resume r : ARRAY_STORAGE.getAll()) {
-            System.out.println(r);
-        }
+        ARRAY_STORAGE.delete("uuid10");
+//        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+//        System.out.println("Size: " + ARRAY_STORAGE.size());
+//
+//        System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
+//
+//        printAll();
+//        ARRAY_STORAGE.delete(r1.getUuid());
+//        printAll();
+//        ARRAY_STORAGE.clear();
+//        printAll();
+//
+//        System.out.println("Size: " + ARRAY_STORAGE.size());
+//
+//        for (int i = 0; i < 10001; i++) {
+//            ARRAY_STORAGE.save(new Resume().setUuid("name" + i));
+//        }
+//    }
+//
+//    static void printAll() {
+//        System.out.println("\nGet All");
+//        for (Resume r : ARRAY_STORAGE.getAll()) {
+//            System.out.println(r);
+//        }
     }
 }
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -4,7 +4,7 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
 
-public class SortedArrayStorage extends AbstractArrayStorage{
+public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void clear() {
@@ -13,17 +13,48 @@ public class SortedArrayStorage extends AbstractArrayStorage{
 
     @Override
     public void update(Resume r) {
+        if (size > 0) {
 
+        } else {
+            System.out.println("Отсутствуют элементы в базе!");
+        }
     }
 
     @Override
     public void save(Resume r) {
-
+        if (size > 0) {
+            int index = Arrays.binarySearch(storage, 0, size, r);
+            int num;
+            if (index < 0) {
+                if (size < storage.length) {
+                    num = -index - 1;
+                    System.arraycopy(storage, num, storage, num + 1, size - num);
+                    storage[num] = r;
+                    size++;
+                } else {
+                    System.out.println(r.getUuid() + "Нет свободной памяти!");
+                }
+            } else {
+                System.out.println(r.getUuid() +" Данный элемент присутсвует в базе!");
+            }
+        } else {
+            storage[0] = r;
+            size++;
+        }
     }
 
     @Override
     public void delete(String uuid) {
-
+        int index = Arrays.binarySearch(storage, 0, size, new Resume().setUuid(uuid));
+        if (index >= 0) {
+            if (size - 1 - index >= 0) {
+                System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
+            }
+            Arrays.fill(storage, size-1, size, null);
+            size--;
+        } else {
+            System.out.println(uuid + " Отсутствует в базе!");
+        }
     }
 
     @Override
