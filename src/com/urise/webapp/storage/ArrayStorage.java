8331aa9a29245948a488c7ca17767417d38ed828
Ilya Stroev <ilya_stroev@mail.ru>
2020-10-27 18:17:54 +0300

    refactor ArrayStorage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,24 +7,19 @@ import com.urise.webapp.model.Resume;
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-
-
+    @Override
     public void save(Resume r) {
-        if (getIndex(r.getUuid()) != -1) {
-            System.out.println("Resume " + r.getUuid() + " already exist");
-        } else if (size >= STORAGE_LIMIT) {
-            System.out.println("Storage overflow");
-        } else {
+        int index = checkResume(r.getUuid());
+        if (index >= 0) {
             storage[size] = r;
             size++;
         }
     }
 
+    @Override
     public void delete(String uuid) {
-        int index = getIndex(uuid);
-        if (index == -1) {
-            System.out.println("Resume " + uuid + " not exist");
-        } else {
+        int index = checkResume(uuid);
+        if (index >= 0) {
             storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
