a990d35885d0be1de96d7472a4d2b5a515d66116
Ilya Stroev <ilya_stroev@mail.ru>
2020-11-17 12:39:15 +0300

    create MapStorageTest and refactor method getAll

--- test/com/urise/webapp/storage/MapStorageTest.java
+++ test/com/urise/webapp/storage/MapStorageTest.java
@@ -4,8 +4,6 @@ import com.urise.webapp.model.Resume;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MapStorageTest extends AbstractStorageTest {
 
     public MapStorageTest() {
@@ -16,7 +14,17 @@ public class MapStorageTest extends AbstractStorageTest {
     public void getAll() {
         Resume[] resumeExpected = storage.getAll();
         Resume[] resumeActual = new Resume[]{resume1, resume2, resume3};
-        Assert.assertArrayEquals(resumeExpected, resumeActual);
-        Assert.a
+        Assert.assertEquals(3,storage.size());
+        int countEquals = 0;
+        for (Resume resume : resumeExpected) {
+            for (Resume value : resumeActual) {
+                if (resume.equals(value)) {
+                    countEquals++;
+                    return;
+                }
+            }
+
+        }
+        Assert.assertEquals(resumeActual.length,countEquals);
     }
 }
